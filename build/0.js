eb9d0005b935bd388bc95317c93a04ced916ebf2
Brian Flannery <brian@union-pos.com>
2017-11-20 05:50:12 -0600

    20 - Environment Variables

--- build/0.js
+++ build/0.js
@@ -1,12 +1,12 @@
-/*! e481c8f */
-webpackJsonp([0],{17:/*!*********************!*\
+/*! 2c80de4 */
+webpackJsonp([0],{18:/*!*********************!*\
   !*** ./app/lazy.js ***!
   \*********************/
-function(o,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(/*! ./lazy.css */21);n.n(r);e.default="Hello from lazy"},20:/*!*************************************************************************************************************************!*\
+function(o,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n(/*! ./lazy.css */22);n.n(r);e.default="Hello from lazy"},21:/*!*************************************************************************************************************************!*\
   !*** ./~/css-loader!./~/postcss-loader/lib?{"plugins":[null]}!./~/postcss-loader/lib?{"plugins":[null]}!./app/lazy.css ***!
   \*************************************************************************************************************************/
-function(o,e,n){(o.exports=n(/*! ../~/css-loader/lib/css-base.js */18)(void 0)).push([o.i,"body {\n    color: blue;\n}\n",""])},21:/*!**********************!*\
+function(o,e,n){(o.exports=n(/*! ../~/css-loader/lib/css-base.js */19)(void 0)).push([o.i,"body {\n    color: blue;\n}\n",""])},22:/*!**********************!*\
   !*** ./app/lazy.css ***!
   \**********************/
-function(o,e,n){var r=n(/*! !../~/css-loader!../~/postcss-loader/lib??ref--4-3!../~/postcss-loader/lib??ref--1!./lazy.css */20);"string"==typeof r&&(r=[[o.i,r,""]]);var t={hmr:!0};t.transform=void 0;n(/*! ../~/style-loader/lib/addStyles.js */19)(r,t);r.locals&&(o.exports=r.locals)}});
+function(o,e,n){var r=n(/*! !../~/css-loader!../~/postcss-loader/lib??ref--4-3!../~/postcss-loader/lib??ref--1!./lazy.css */21);"string"==typeof r&&(r=[[o.i,r,""]]);var t={hmr:!0};t.transform=void 0;n(/*! ../~/style-loader/lib/addStyles.js */20)(r,t);r.locals&&(o.exports=r.locals)}});
 //# sourceMappingURL=0.js.map
--- build/app.js
+++ build/app.js
@@ -1,18 +1,21 @@
-/*! e481c8f */
-webpackJsonp([1],{16:/*!**********************!*\
-  !*** ./app/index.js ***!
-  \**********************/
-function(n,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=t(/*! react */10),c=(t.n(o),t(/*! font-awesome/css/font-awesome.css */7)),i=(t.n(c),t(/*! purecss */9)),u=(t.n(i),t(/*! ./main.css */8)),a=(t.n(u),t(/*! ./component */6));document.body.appendChild(t.i(a.a)())},6:/*!**************************!*\
+/*! 2c80de4 */
+webpackJsonp([1],[,,,,,,/*!**************************!*\
   !*** ./app/component.js ***!
   \**************************/
-function(n,e,t){"use strict";e.a=function(){var n=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"Hello world",e=document.createElement("div");return e.className="fa fa-hand-spock-o fa-1g",e.innerHTML=n,e.onclick=function(){t.e(0).then(t.bind(null,/*! ./lazy */17)).then(function(n){e.textContent=n.default}).catch(function(n){console.error(n)})},e}},7:/*!*********************************************!*\
+function(n,t,e){"use strict";t.a=function(){var n=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"Hello world",t=document.createElement("div");return t.className="fa fa-hand-spock-o fa-1g",t.innerHTML=n,t.onclick=function(){e.e(0).then(e.bind(null,/*! ./lazy */18)).then(function(n){t.textContent=n.default}).catch(function(n){console.error(n)})},t}},/*!**********************!*\
+  !*** ./app/shake.js ***!
+  \**********************/
+function(n,t,e){"use strict";e.d(t,"a",function(){return o});var o=function(){return console.log("bake")}},/*!*********************************************!*\
   !*** ./~/font-awesome/css/font-awesome.css ***!
   \*********************************************/
-function(n,e){},8:/*!**********************!*\
+function(n,t){},/*!**********************!*\
   !*** ./app/main.css ***!
   \**********************/
-function(n,e){},9:/*!**************************************!*\
+function(n,t){},/*!**************************************!*\
   !*** ./~/purecss/build/pure-min.css ***!
   \**************************************/
-function(n,e){}},[16]);
+function(n,t){},,,,,,,/*!**********************!*\
+  !*** ./app/index.js ***!
+  \**********************/
+function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=e(/*! react */11),c=(e.n(o),e(/*! font-awesome/css/font-awesome.css */8)),i=(e.n(c),e(/*! purecss */10)),u=(e.n(i),e(/*! ./main.css */9)),a=(e.n(u),e(/*! ./component */6)),r=e(/*! ./shake */7);e.i(r.a)(),document.body.appendChild(e.i(a.a)())}],[17]);
 //# sourceMappingURL=app.js.map
